07b264b8e3b6d45210c541e9f41f922a759bb4ae
Hamed-Traore <91626326+Hamed-Traore@users.noreply.github.com>
2022-12-06 10:55:26 +0000

    heroku

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,7 +1,13 @@
 <?php
 //Database credentials
-define('SERVER','localhost');
-define('HOST','root');// id19735886_hamed
-define('PASSWD','');//k|r(6)4O=uZ{<g%>
-define('DB', 'as_fashion_db');
+// define('SERVER','localhost');
+// define('HOST','root');// id19735886_hamed
+// define('PASSWD','');//k|r(6)4O=uZ{<g%>
+// define('DB', 'as_fashion_db');
+
+//Database heroku credentials
+define('SERVER','us-cdbr-east-06.cleardb.net');
+define('HOST','bd775049842a98');
+define('PASSWD','e7e7881e');
+define('DB', 'heroku_94a1840aa90507d');
 ?>
